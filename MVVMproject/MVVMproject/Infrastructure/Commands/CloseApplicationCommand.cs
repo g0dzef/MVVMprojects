48b2018f57ddfeec06eb7613d53a595ef7885d28
g0dzef <mr.helios.a@mail.ru>
2022-07-25 22:12:40 +0300

    add CloseApplicationCommand

---
+++ MVVMproject/MVVMproject/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,17 @@
+﻿using MVVMproject.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVMproject.Infrastructure.Commands
+{
+    public class CloseApplicationCommand : BaseCommand
+    {
+        public override bool CanExecute(object? parameter) => true;
+
+        public override void Execute(object? parameter) => Application.Current.Shutdown();
+    }
+}
--- MVVMproject/MVVMproject/MVVM/ViewModels/MainWindowViewModel.cs
+++ MVVMproject/MVVMproject/MVVM/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,12 @@
-﻿using MVVMproject.MVVM.ViewModels.Base;
+﻿using MVVMproject.Infrastructure.Commands;
+using MVVMproject.MVVM.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace MVVMproject.MVVM.ViewModels
 {
@@ -17,5 +20,20 @@ namespace MVVMproject.MVVM.ViewModels
             set { Set(ref _title, value); }
         }
 
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand { get; }
+
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            CloseApplicationCommand = new RelayCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+        }
     }
 }

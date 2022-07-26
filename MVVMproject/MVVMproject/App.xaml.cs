5c550dab527523326726010162d3b67809765408
g0dzef <mr.helios.a@mail.ru>
2022-07-26 19:34:51 +0300

    Add host

--- MVVMproject/MVVMproject/App.xaml.cs
+++ MVVMproject/MVVMproject/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MVVMproject.MVVM.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +16,30 @@ namespace MVVMproject
     /// </summary>
     public partial class App : Application
     {
+        private static IHost _host;
+        public static IHost Host => _host ?? Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var host = Host;
+
+            await host.StartAsync().ConfigureAwait(false);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            var host = Host;
+
+            await host.StopAsync().ConfigureAwait(false);
+            host.Dispose();
+            _host = null;
+        }
+
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<MainWindowViewModel>();
+        }
     }
 }
---
+++ MVVMproject/MVVMproject/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMproject
+{
+    public static class Program
+    {
+      public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var hostBuilder = Host.CreateDefaultBuilder(args);
+
+            hostBuilder.UseContentRoot(Environment.CurrentDirectory);
+            hostBuilder.ConfigureAppConfiguration((host, cfg) =>
+            {
+                cfg.SetBasePath(Environment.CurrentDirectory);
+                cfg.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            });
+
+            hostBuilder.ConfigureServices(App.ConfigureServices);
+
+            return hostBuilder;
+        }
+    }
+}
